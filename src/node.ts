fc7e9e3319db88551a14045ba70d44438cae2848
Philipp Viereck <philipp@mailbean.io>
2023-09-30 12:38:46 +0200

    fix for cloudflare

--- src/node.ts
+++ src/node.ts
@@ -2,7 +2,6 @@ import type { Config, Client } from "./api.js";
 import { LibsqlError } from "./api.js";
 import type { ExpandedConfig } from "./config.js";
 import { expandConfig } from "./config.js";
-import { _createClient as _createSqlite3Client } from "./sqlite3.js";
 import { _createClient as _createWsClient } from "./ws.js";
 import { _createClient as _createHttpClient } from "./http.js";
 
@@ -13,15 +12,18 @@ export * from "./api.js";
  * You must pass at least an `url` in the {@link Config} object.
  */
 export function createClient(config: Config): Client {
-    return _createClient(expandConfig(config, true));
+  return _createClient(expandConfig(config, true));
 }
 
 function _createClient(config: ExpandedConfig) {
-    if (config.scheme === "wss" || config.scheme === "ws") {
-        return _createWsClient(config);
-    } else if (config.scheme === "https" || config.scheme === "http") {
-        return _createHttpClient(config);
-    } else {
-        return _createSqlite3Client(config);
-    }
+  if (config.scheme === "wss" || config.scheme === "ws") {
+    return _createWsClient(config);
+  } else if (config.scheme === "https" || config.scheme === "http") {
+    return _createHttpClient(config);
+  } else {
+    throw new LibsqlError(
+      "This build does not support local libsql files",
+      "URL_SCHEME_NOT_SUPPORTED"
+    );
+  }
 }
